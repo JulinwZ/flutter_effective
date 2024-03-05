bf1d1e174ed15dc889a5789e7c14a9068d723728
JulinwZ <julinwz@gmail.com>
2024-03-05 07:26:27 +0600

    удалил ненужные пока что папки и импортировал menu_screen в app.dart

---
+++ lib/src/features/menu/view/menu_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Меню'),
+      ),
+      body: Center(
+        child: Text('Здесь будет ваше меню'),
+      ),
+    );
+  }
+}
